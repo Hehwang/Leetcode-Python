2f9e2cc0ba93a4e22e880e4d4d1b059a60209bb3
Hehwang <565182282@qq.com>
2017-09-29 23:55:08 +0800

    ~

---
+++ code/309 Best Time to Buy and Sell Stock with Cooldown.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices)==0:
+            return 0
+        buy=[0]*len(prices)
+        sell=[0]*len(prices)
+        buy[0]=-prices[0]
+        for i in range(1,len(prices)):
+            price=prices[i]
+            buy[i]=max(buy[i-1],sell[i-2]-price)
+            sell[i]=max(sell[i-1],buy[i-1]+price)
+        return sell[-1]
