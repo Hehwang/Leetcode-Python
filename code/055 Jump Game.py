756de9550435f9b499af2fb9a114281eabce5b92
Hehwang <565182282@qq.com>
2017-10-15 23:49:48 +0800

    easy

---
+++ code/055 Jump Game.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        maxRight=0
+        i=0
+        while maxRight<len(nums)-1:
+            maxRight=max(maxRight,nums[i]+i)
+            if maxRight<=i:
+                return False
+            i+=1
+        return True
