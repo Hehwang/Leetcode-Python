a44b9f358e9bf22a8385042467d2cb83819f2b64
Hehwang <565182282@qq.com>
2017-11-03 14:20:06 +0800

    Medium

---
+++ code/073 Set Matrix Zeroes.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        row=[False]*len(matrix)
+        col=[False]*len(matrix[0])
+        for i in range(len(row)):
+            for j in range(len(col)):
+                if matrix[i][j]==0:
+                    row[i]=True
+                    col[j]=True
+        for i in range(len(row)):
+            for j in range(len(col)):
+                if row[i] or col[j]:
+                    matrix[i][j]=0
